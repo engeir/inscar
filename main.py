60b72326f08a0cbd8bfa83889dbd5e054ef72051
Eirik Rolland Enger <eirroleng@gmail.com>
2020-01-14 13:05:12 +0100

    Minor fixes

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 """Main script.
 """
-from functions import *
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+import functions as func
 
-B = 5e-4
-f0 = 500e6
-f_ion = np.linspace(- 150e2, 150e2, 121)
+B = 5e-4  # Magnetic field strength [T]
+f0 = 500e6  # Radar frequency [Hz]
+f_ion = np.linspace(- 150e2, 150e2, 121)  # Ion frequency [Hz]
 f_ion += 1
-mi = 16
-ne = 2e11
-Nu_e = 1000
-Nu_i = 100
-T_e = 2500
-theta = 0.1
-Ti = 1500
+mi = 16  # Ion mass in atomic mass units [u]
+ne = 2e11  # Electron number density [m^(-3)]
+Nu_e = 1000  # Electron collision frequency [Hz]
+Nu_i = 100  # Ion collision frequency [Hz]
+T_e = 2500  # Electron temperature [K]
+Ti = 1500  # Ion temperature [K]
+theta = 0.1  # Aspect angle
 
-Is = isspec_ne(f_ion + 1, f0, ne, T_e, Nu_e, mi, Ti, Nu_i, B, theta)
+Is = func.isspec_ne(f_ion + 1, f0, ne, T_e, Nu_e, mi, Ti, Nu_i, B, theta)
 plt.figure()
 plt.plot(f_ion, Is)
 plt.show()
