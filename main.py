a0e72cad115fac5031f86f3abcf9db32eb166ca9
Eirik Rolland Enger <eirroleng@gmail.com>
2020-01-23 16:35:03 +0100

    Vectorized version using chirp-z transform to calculate Gordeyev integral

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
 """Main script for calculating the IS spectrum based on realistic parameters.
 """
 
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 
 import functions as func
+import config as cf
 
-B = 5e-4  # Magnetic field strength [T]
-f0 = 500e6  # Radar frequency [Hz]
-f_ion_max = 150e2
-f_ion = np.linspace(- f_ion_max, f_ion_max, 121)  # Ion frequency [Hz]
-f_ion += 1
-mi = 16  # Ion mass in atomic mass units [u]
-ne = 2e11  # Electron number density [m^(-3)]
-Nu_e = 1000  # Electron collision frequency [Hz]
-Nu_i = 100  # Ion collision frequency [Hz]
-T_e = 2500  # Electron temperature [K]
-T_i = 1500  # Ion temperature [K]
-theta = 0.1  # Pitch angle
 
-Is = func.isspec_ne(f_ion + 1, f0, ne, T_e, Nu_e, mi, T_i, Nu_i, B, theta)
-plt.figure()
-plt.plot(f_ion, abs(Is))
-plt.show()
+def decide_on_params(default=True):
+    if default == True:
+        return [cf.F_ION + 1, cf.F0, cf.NE, cf.T_E, cf.NU_E, cf.MI, cf.T_I, cf.NU_I, cf.B, cf.THETA]
+    while 1:
+        try:
+            use_default = input(
+                'Do you want to use default parameters? (y/yes or n/no)\t').lower()
+        except Exception:
+            print('Input must be a string.')
+        else:
+            if use_default in ['y', 'yes']:
+                return [cf.F_ION + 1, cf.F0, cf.NE, cf.T_E, cf.NU_E, cf.MI, cf.T_I, cf.NU_I, cf.B, cf.THETA]
+            elif use_default in ['n', 'no']:
+                text = 'Set the values of\n f_min, f_max, f_steps, f0, n_e, Te, Nu_e, mi, T_i, Nu_i, B, theta\nin that order, separated by a comma.\n '
+                parameters = list(map(float, input(f'{text}').split(', ')))
+                if len(parameters) == 12:
+                    f_ion = np.linspace(
+                        parameters[0], parameters[1], parameters[2])
+                    parameters[:3] = [f_ion]
+                    return parameters
+            print('You must type in either of "y/yes/n/no".')
+
+
+def plot_IS_spectrum():
+    args = decide_on_params()
+    t0 = time.perf_counter()
+    Is, w = func.isspec_ne(*args)
+    print(f'RunTime = {time.perf_counter() - t0}')
+    plt.figure()
+    plt.title('ISR spectrum')
+    plt.plot(w / (2 * np.pi * cf.N_POINTS), abs(Is), 'r')
+    plt.plot(- w / (2 * np.pi * cf.N_POINTS), abs(Is), 'r')
+    plt.show()
+
+
+if __name__ == '__main__':
+    plot_IS_spectrum()
