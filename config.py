6d49f0b62287e9dac952dc4b2a826435243a85ca
Eirik Rolland Enger <eirroleng@gmail.com>
2020-01-24 11:09:32 +0100

    Minimal working code for ISR spectrum

--- config.py
+++ config.py
@@ -12,12 +12,10 @@ Q_E = 1.602176565e-19  # elementary charge [C]
 K_B = 1.380662e-23  # Boltzmann constant [J/K]
 MY_0 = 4 * np.pi * 1e-7  # Permeability [Vs/Am]
 
-UPPER_LIMIT = 1e1
 N_POINTS = 1e6
 T_MAX = 1e3
 
 # === Input parameters ===
-LIMIT = 1500  # Subintervals in integration method
 B = 5e-4  # Magnetic field strength [T]
 F0 = 500e6  # Radar frequency [Hz]
 F_ION_MAX = 6e6  # F0 / F_ION_MAX > 15 000
@@ -29,4 +27,4 @@ NU_E = 1000  # Electron collision frequency [Hz]
 NU_I = 100  # Ion collision frequency [Hz]
 T_E = 2500  # Electron temperature [K]
 T_I = 1500  # Ion temperature [K]
-THETA = 0.1  # 45 * np.pi / 180  # Pitch angle
+THETA = 45 * np.pi / 180  # Pitch angle
