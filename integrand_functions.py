c8ab74a2ada8f97a6cc8d2d4cb6945549b412077
Eirik Rolland Enger <eirroleng@gmail.com>
2020-02-18 16:17:35 +0100

    Attempt to add new dist func from Ziebell (2017)

--- integrand_functions.py
+++ integrand_functions.py
@@ -5,6 +5,27 @@ import numpy as np
 import config as cf
 
 
+def ziebell_z_func(kappa, m, xi):
+    F = sps.hyp2f1(1, 2 * kappa - 2 * m, kappa + m +
+                   1, .5 * (1 + 1j * xi / kappa**.5))
+    num = 1j * sps.gamma(kappa) * sps.gamma(kappa + m + 1 / 2)
+    den = kappa**.5 * sps.gamma(kappa - .5) * sps.gamma(kappa + m + 1)
+    Z = num / den * F
+    return Z
+
+
+def two_p_isotropic_kappa(params):
+    w_bk = ((cf.KAPPA - 3 / 2) / cf.KAPPA)**.5 * params['T'] * const.k / params['m']
+    zbn = cf.w / (cf.K_RADAR * np.cos(cf.I_P['THETA']) * w_bk)  # (\zeta_\beta^0)
+    D = 2 * params['w_c']**2 / cf.w**2 * zbn**2 * \
+        ((cf.KAPPA - .5) / cf.KAPPA + zbn * ziebell_z_func(cf.KAPPA, 1, zbn))
+    l_D2 = const.epsilon_0 * params['T'] / (cf.I_P['NE'] * const.elementary_charge**2) * (cf.KAPPA - 3 / 2) / (cf.KAPPA - 1 / 2)
+    A = 1 / (cf.K_RADAR**2 * l_D2)
+    G = 1j * (1 - A * D) / cf.w
+    F = 1 - (1j * cf.w + params['nu']) * G
+    return F
+
+
 def z_func(y, w_c, m, T):
     theta_2 = 2 * ((cf.KAPPA - 3 / 2) / cf.KAPPA) * T * const.k / m
     Z = (2 * cf.KAPPA)**(1 / 2) * \
