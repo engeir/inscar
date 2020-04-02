4c470e7dafb5d11d0c2213f67399587e96fd2c66
Eirik Rolland Enger <eirroleng@gmail.com>
2020-04-02 10:45:47 +0200

    improved testing

--- test_ISR.py
+++ test_ISR.py
@@ -1,8 +1,18 @@
 import tool
 
 
-def test_isr_spectrum():
-    a, b = tool.isr_spectrum('kappa', kappa=6)
+def test_isr_spectrum1():
+    a, b = tool.isr_spectrum('maxwell', kappa=6)
+    assert isinstance(a, type(b)) and a.shape == b.shape
+
+
+def test_isr_spectrum2():
+    a, b = tool.isr_spectrum('kappa', kappa=4)
+    assert isinstance(a, type(b)) and a.shape == b.shape
+
+
+def test_isr_spectrum3():
+    a, b = tool.isr_spectrum('long_calc', kappa=6)
     assert isinstance(a, type(b)) and a.shape == b.shape
 
 
