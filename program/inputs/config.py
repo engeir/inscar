41b291c34561524bdf7a641cfc30aacd1a99adf2
Eirik Rolland Enger <eirroleng@gmail.com>
2020-04-17 09:58:14 +0200

    added description of suprathermal electron temp

--- program/inputs/config.py
+++ program/inputs/config.py
@@ -21,15 +21,15 @@ if os.path.basename(os.path.realpath(sys.argv[0])) != 'main.py':
     V_N_POINTS = 1e1
 else:
     F_N_POINTS = 1e3  # Number of sample points in frequency
-    Y_N_POINTS = 1e3  # Number of sample points in integral variable
-    V_N_POINTS = 5e4  # Number of sample points in velocity integral variable
+    Y_N_POINTS = 5e5  # Number of sample points in integral variable
+    V_N_POINTS = 6e4  # Number of sample points in velocity integral variable
 # Adds one sample to get an even number of bins, which in
 # turn give better precision in the Simpson integration.
 Y_N_POINTS += 1
 V_N_POINTS += 1
 Y_MAX_e = 1.5e-4  # Upper limit of integration (= infinity)
 Y_MAX_i = 1.5e-2
-V_MAX = 1e7
+V_MAX = 3e7
 ORDER = 3
 
 # === Input parameters ===
@@ -42,6 +42,7 @@ ORDER = 3
 # NU_I -- Ion collision frequency [Hz]
 # T_E -- Electron temperature [K]
 # T_I -- Ion temperature [K]
+# T_ES -- Temperature of suprathermal electrons in the gauss_shell VDF [K]
 # THETA -- Pitch angle [1]
 
 # For seeing gyro lines
@@ -51,8 +52,10 @@ ORDER = 3
 # I_P = {'B': 35000e-9, 'F0': 430e6, 'F_MAX': 1.5e6, 'MI': 16, 'NE': 2e10,
 #        'NU_E': 0, 'NU_I': 0, 'T_E': 1000, 'T_I': 1000, 'THETA': 0 * np.pi / 180}
 # High frequency plasma lines
-I_P = {'B': 5e-4, 'F0': 933e6, 'F_MAX': 1e7, 'MI': 16, 'NE': 2e11,
-       'NU_E': 1000, 'NU_I': 0, 'T_E': 4000, 'T_I': 2000, 'THETA': 45 * np.pi / 180}
+# I_P = {'B': 5e-4, 'F0': 933e6, 'F_MAX': 1.25e7, 'MI': 16, 'NE': 2e11, 'NU_E': 1000, 'NU_I': 0,
+#        'T_E': 10000, 'T_I': 2000, 'T_ES': 90000, 'THETA': 45 * np.pi / 180}
+I_P = {'B': 5e-4, 'F0': 430e6, 'F_MAX': 7e6, 'MI': 16, 'NE': 2e11, 'NU_E': 1000, 'NU_I': 0,
+       'T_E': 2000, 'T_I': 1000, 'T_ES': 90000, 'THETA': 40 * np.pi / 180}
 
 # DO NOT EDIT
 K_RADAR = - 2 * I_P['F0'] * 2 * np.pi / const.c  # Radar wavenumber
