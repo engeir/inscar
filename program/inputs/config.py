eb79415e3f00d0e02220160222376e2b24bf1572
Eirik Rolland Enger <eirroleng@gmail.com>
2020-04-23 16:50:35 +0200

    update on 23_4

--- program/inputs/config.py
+++ program/inputs/config.py
@@ -20,9 +20,9 @@ if os.path.basename(os.path.realpath(sys.argv[0])) != 'main.py':
     Y_N_POINTS = 1e1
     V_N_POINTS = 1e1
 else:
-    F_N_POINTS = 1e3  # Number of sample points in frequency
+    F_N_POINTS = 1e4  # Number of sample points in frequency
     Y_N_POINTS = 5e3  # Number of sample points in integral variable
-    V_N_POINTS = 6e3  # Number of sample points in velocity integral variable
+    V_N_POINTS = 6e4  # Number of sample points in velocity integral variable
 # Adds one sample to get an even number of bins, which in
 # turn give better precision in the Simpson integration.
 Y_N_POINTS += 1
@@ -48,17 +48,17 @@ ORDER = 3
 el_temp = []
 e_t_0 = 2000
 e_t = 1000
-for i in range(2):
+for i in range(9):
     el_temp.append(e_t_0 + e_t * i)
 # For seeing gyro lines
-# I_P = {'B': 35000e-9, 'F0': 430e6, 'F_MAX': 2e6, 'MI': 29, 'NE': 2e10,
-#        'NU_E': 0, 'NU_I': 0, 'T_E': 200, 'T_I': 200, 'THETA': 45 * np.pi / 180}
+I_P = {'B': 35000e-9, 'F0': 430e6, 'F_MAX': 3e3, 'MI': 29, 'NE': 2e10,
+       'NU_E': 0, 'NU_I': 0, 'T_E': 200, 'T_I': 200, 'T_ES': 10000, 'THETA': 45 * np.pi / 180}
 # For same plots as Hagfors
 # I_P = {'B': 35000e-9, 'F0': 430e6, 'F_MAX': 1.5e6, 'MI': 16, 'NE': 2e10,
 #        'NU_E': 0, 'NU_I': 0, 'T_E': 1000, 'T_I': 1000, 'THETA': 0 * np.pi / 180}
 # High frequency plasma lines
-I_P = {'B': 5e-4, 'F0': 933e6, 'F_MAX': 1e7, 'MI': 16, 'NE': 2e11, 'NU_E': 0, 'NU_I': 0,
-       'T_E': 2000, 'T_I': 2000, 'T_ES': 90000, 'THETA': 0}
+# I_P = {'B': 5e-4, 'F0': 933e6, 'F_MAX': 1e7, 'MI': 16, 'NE': 2e11, 'NU_E': 0, 'NU_I': 0,
+#        'T_E': 2000, 'T_I': 2000, 'T_ES': 90000, 'THETA': 0}
 # I_P = {'B': 5e-4, 'F0': 430e6, 'F_MAX': 1e4, 'MI': 16, 'NE': 2e11, 'NU_E': 1000, 'NU_I': 0,
 #        'T_E': 2000, 'T_I': 1000, 'T_ES': 90000, 'THETA': 40 * np.pi / 180}
 
