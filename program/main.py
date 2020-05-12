fixed: error in char.vel./Debye length of a_vdf method, now with correct scaling

program/main.py
@@ -407,34 +407,29 @@ class Simulation:
         ridge = []
         sys_set = {'B': 5e-4, 'MI': 16, 'NE': 2e11, 'NU_E': 0, 'NU_I': 0, 'T_E': 5000, 'T_I': 2000, 'T_ES': 90000,
                    'THETA': 40 * np.pi / 180, 'Z': 599, 'mat_file': 'fe_zmuE-01.mat'}
-        params = {'kappa': 3, 'vdf': 'kappa', 'area': False}
+        params = {'kappa': 8, 'vdf': 'kappa', 'area': False}
         # Ridge 1
-        # self.f, s, meta_data = isr.isr_spectrum('maxwell', sys_set, **params)
-        # ridge.append(s)
-        # self.meta_data.append(meta_data)
         self.f, s, meta_data = isr.isr_spectrum('kappa', sys_set, **params)
         ridge.append(s)
         self.meta_data.append(meta_data)
         self.f, s, meta_data = isr.isr_spectrum('a_vdf', sys_set, **params)
         ridge.append(s)
         self.meta_data.append(meta_data)
-        self.data.append(ridge)
-        # # Ridge 2
-        # ridge = []
-        # self.f, s, meta_data = isr.isr_spectrum('kappa', sys_set, **params)
-        # ridge.append(s)
-        # self.meta_data.append(meta_data)
-        # params['vdf'] = 'kappa'
-        # self.f, s, meta_data = isr.isr_spectrum('a_vdf', sys_set, **params)
-        # ridge.append(s)
-        # self.meta_data.append(meta_data)
-        # self.data.append(ridge)
+        self.f, s, meta_data = isr.isr_spectrum('maxwell', sys_set, **params)
+        ridge.append(s)
+        self.meta_data.append(meta_data)
+        params['vdf'] = 'maxwell'
+        self.f, s, meta_data = isr.isr_spectrum('a_vdf', sys_set, **params)
+        ridge.append(s)
+        self.meta_data.append(meta_data)
 
-        self.legend_txt.append('Maxwell')
+        self.data.append(ridge)
         self.legend_txt.append('Kappa')
         self.legend_txt.append('Kappa Long calc')
-        self.ridge_txt.append('Maxwell')
-        self.ridge_txt.append('Kappa')
+        self.legend_txt.append('Maxwell')
+        self.legend_txt.append('Maxwell Long calc')
+        self.ridge_txt.append('All')
+        # self.ridge_txt.append('Kappa')
 
             # self.ridge_txt.append(f'${H}$ km')
             # for m in ToD:
@@ -461,10 +456,10 @@ class Simulation:
             self.plot.plot_ridge(self.f, self.data, 'plot', self.legend_txt, self.ridge_txt)
             self.plot.plot_ridge(self.f, self.data, 'semilogy', self.legend_txt, self.ridge_txt)
         """
-        self.plot.plasma = True
         # self.plot.plot_normal(self.f, self.data[0], 'plot', self.legend_txt)
         # self.plot.plot_normal(self.f, self.data[0], 'semilogy', self.legend_txt)
         self.plot.plot_ridge(self.f, self.data, 'plot', self.legend_txt, self.ridge_txt)
+        self.plot.plasma = True
         self.plot.plot_ridge(self.f, self.data, 'semilogy', self.legend_txt, self.ridge_txt)
         self.plot.plot_ridge(self.f, self.data, 'loglog', self.legend_txt, self.ridge_txt)
 
@@ -485,6 +480,5 @@ class Simulation:
 
 
 if __name__ == '__main__':
-    # TODO: answer, what happens to the Debye length when we use different VDFs?
     sim = Simulation()
     sim.run()

program/utils/spectrum_calculation.py
@@ -44,8 +44,8 @@ def isr_spectrum(version, system_set, kappa=None, vdf=None, area=False):
     W_c = w_ion_gyro(np.linalg.norm([sys_set['B']], 2), M_i)
 
     # Ions
-    params = {'THETA': sys_set['THETA'], 'nu': sys_set['NU_I'], 'm': M_i, 'T': sys_set['T_I'],
-              'w_c': W_c, 'kappa': kappa, 'vdf': vdf}
+    params = {'THETA': sys_set['THETA'], 'nu': sys_set['NU_I'], 'm': M_i, 'T': sys_set['T_I'], 'w_c': W_c}
+            #   , 'kappa': kappa, 'vdf': vdf}
     y = np.linspace(0, cf.Y_MAX_i**(1 / cf.ORDER), int(cf.Y_N_POINTS), dtype=np.double)**cf.ORDER
     f_ion = intf.INT_MAXWELL()
     f_ion.initialize(y, params)
@@ -60,9 +60,16 @@ def isr_spectrum(version, system_set, kappa=None, vdf=None, area=False):
         const.m_e, sys_set['T_E'], sys_set['NU_E'], y, function=func, kappa=kappa)
 
     Xp_i = np.sqrt(
-        1 / (2 * L_Debye(sys_set['NE'], sys_set['T_E'], kappa=3)**2 * cf.K_RADAR**2))
-    Xp_e = np.sqrt(
-        1 / (2 * L_Debye(sys_set['NE'], sys_set['T_E'], kappa=3)**2 * cf.K_RADAR**2))
+        1 / (2 * L_Debye(sys_set['NE'], sys_set['T_E'], kappa=None)**2 * cf.K_RADAR**2))
+    if func.the_type == 'a_vdf':
+        Xp_e = np.sqrt(
+            1 / (2 * L_Debye(sys_set['NE'], sys_set['T_E'], char_vel=func.char_vel)**2 * cf.K_RADAR**2))
+    elif func.the_type == 'kappa':
+        Xp_e = np.sqrt(
+            1 / (2 * L_Debye(sys_set['NE'], sys_set['T_E'], kappa=kappa)**2 * cf.K_RADAR**2))
+    else:
+        Xp_e = np.sqrt(
+            1 / (2 * L_Debye(sys_set['NE'], sys_set['T_E'])**2 * cf.K_RADAR**2))
 
     # f_scaled = cf.f
     # In case we have \omega = 0 in our frequency array, we just ignore this warning message
@@ -82,7 +89,7 @@ def isr_spectrum(version, system_set, kappa=None, vdf=None, area=False):
     return cf.f, Is, dict(sys_set, **p)
 
 
-def L_Debye(*args, kappa=None):
+def L_Debye(*args, kappa=None, char_vel=None):
     """Calculate the Debye length.
 
     Input args may be
@@ -107,12 +114,15 @@ def L_Debye(*args, kappa=None):
     Ep0 = 1e-09 / 36 / np.pi
 
     if nargin < 3:
-        if kappa is None:
-            LD = np.sqrt(Ep0 * const.k * T_e /
-                         (max(0, n_e) * const.e**2))
-        else:
+        if kappa is not None:
             LD = np.sqrt(Ep0 * const.k * T_e / (max(0, n_e) * const.e**2)
                          ) * np.sqrt((kappa - 3 / 2) / (kappa - 1 / 2))
+        elif char_vel is not None:
+            LD = np.sqrt(Ep0 * const.k * T_e / (max(0, n_e) * const.e**2)
+                         ) * np.sqrt(char_vel)
+        else:
+            LD = np.sqrt(Ep0 * const.k * T_e /
+                         (max(0, n_e) * const.e**2))
     else:
         LD = np.sqrt(Ep0 * const.k /
                      ((max(0, n_e) / T_e + max(0, n_e) / T_i) / const.e**2))