3b4de9c9fea3b7c2297be67d5629dcc301bd9636
eirik enger <eirroleng@gmail.com>
2020-05-26 12:06:54 +0000

    sync

--- program/utils/hello_kitty.py
+++ program/utils/hello_kitty.py
@@ -42,8 +42,8 @@ class HelloKitty:
     def __init__(self):
         # self.Z = np.arange(100, 350, 50)
         # self.Z = np.linspace(2e10, 6e11, 70)
-        self.Z = np.linspace(2e10, 6e11, 40)
-        self.A = 45 + 15 * np.cos(np.linspace(0, np.pi, 20))  # 25))
+        self.Z = np.linspace(2e10, 6e11, 80)
+        self.A = 45 + 15 * np.cos(np.linspace(0, np.pi, 40))  # 25))
         self.g = np.zeros((len(self.Z), len(self.A)))
         self.dots = [[], []]
         self.meta = []
@@ -57,7 +57,7 @@ class HelloKitty:
         #            'THETA': 40 * np.pi / 180, 'Z': 599, 'mat_file': 'fe_zmuE-07.mat'}
         # params = {'kappa': 8, 'vdf': 'gauss_shell', 'area': False}
         # With real_data, f_0 = 430e6, NE = [2e10, 6e11], 1e4, 4e5, 1e4
-        sys_set = {'B': 35000e-9, 'MI': 16, 'NE': 2e10, 'NU_E': 100, 'NU_I': 0, 'T_E': 1500, 'T_I': 1000, 'T_ES': 90000,
+        sys_set = {'B': 35000e-9, 'MI': 16, 'NE': 2e10, 'NU_E': 100, 'NU_I': 100, 'T_E': 2000, 'T_I': 1500, 'T_ES': 90000,
                    'THETA': 60 * np.pi / 180, 'Z': 599, 'mat_file': 'fe_zmuE-07.mat'}
         params = {'kappa': 8, 'vdf': 'real_data', 'area': False}
         with tqdm(total=len(self.Z) * len(self.A)) as pbar:
@@ -71,7 +71,6 @@ class HelloKitty:
                     sys.stdout = f
                     f, s, meta_data = isr.isr_spectrum('a_vdf', sys_set, **params)
                     sys.stdout = old_stdout
-                    self.meta.append(meta_data)
                     if self.check_energy(f, s, a):
                         self.dots[0].append(j)
                         self.dots[1].append(z)
@@ -80,6 +79,7 @@ class HelloKitty:
                     self.g[i, j] = res
                     # self.g[i, j] = np.max(s)
                     pbar.update(1)
+        self.meta.append(meta_data)
 
     @staticmethod
     def check_energy(f, s, deg):
@@ -138,7 +138,7 @@ class HelloKitty:
             metadata['Subject'] = f"Plasma line power as a function of electron number density and aspect angle."
             metadata['Keywords'] = f'{self.meta}'
             metadata['ModDate'] = datetime.datetime.today()
-            pdffig.attach_note('using 10 pitch, 10percent power')
+            pdffig.attach_note('using 10 pitch, 50percent power')
             plt.savefig(pdffig, bbox_inches='tight', format='pdf', dpi=600)
             pdffig.close()
             plt.savefig(f'{save_path}.pgf', bbox_inches='tight')
