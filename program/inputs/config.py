a631a6eb8389ac1f1609924835d73abb38874ed8
eirik enger <eirroleng@gmail.com>
2020-06-11 12:07:08 +0000

    new figures

--- program/inputs/config.py
+++ program/inputs/config.py
@@ -17,7 +17,7 @@ if os.path.basename(os.path.realpath(sys.argv[0])) != 'main.py':
     V_N_POINTS = 1e1
 else:
     F_N_POINTS = 1e3  # Number of sample points in frequency
-    Y_N_POINTS = 8e4  # Number of sample points in integral variable
+    Y_N_POINTS = 8e6  # Number of sample points in integral variable
     V_N_POINTS = 4e4  # Number of sample points in velocity integral variable
 # Adds one sample to get an even number of bins, which in
 # turn give better precision in the Simpson integration.
@@ -27,7 +27,7 @@ Y_MAX_e = 1.5e-4  # Upper limit of integration (= infinity)
 Y_MAX_i = 1.5e-2
 # Based on E = 110 eV -> 6.22e6 m/s
 V_MAX = 6e6
-ORDER = 5
+ORDER = 3
 
 I_P = {'F_MIN': - 2e6, 'F_MAX': 12e6}
 f = np.linspace(I_P['F_MIN'], I_P['F_MAX'], int(F_N_POINTS))
