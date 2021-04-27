61450e87fa2c8fd95c21ad8ace372de02d423739
engeir <eirroleng@gmail.com>
2021-04-27 23:04:42 +0200

    Fix default experiment on master branch

--- program/inputs/config.py
+++ program/inputs/config.py
@@ -15,7 +15,7 @@ if os.path.basename(os.path.realpath(sys.argv[0])) != "main.py":
     Y_N_POINTS = 1e1
     V_N_POINTS = 1e1
 else:
-    F_N_POINTS = 1e5  # Number of sample points in frequency, $N_f$
+    F_N_POINTS = 1e4  # Number of sample points in frequency, $N_f$
     Y_N_POINTS = 8e4  # Number of sample points in integral variable, $N_y$
     V_N_POINTS = 4e4  # Number of sample points in velocity integral variable, $N_v$
 # Adds one sample to get an even number of bins, which in
