cab1924193d28f37a8356d70ae28e4403791b187
engeir <eirroleng@gmail.com>
2022-05-26 10:41:31 +0200

    Fix docstring into numpy style

--- src/isr_spectrum/vdfs.py
+++ src/isr_spectrum/vdfs.py
@@ -15,45 +15,35 @@ from isr_spectrum import config
 class Vdf(ABC):
     """Base class for a VDF object.
 
-    Arguments:
-        ABC {class} -- abstract base class that all VDF objects inherit from
+    Parameters
+    ----------
+    ABC:
+        Abstract base class that all Vdf objects inherit from
     """
 
     @abstractmethod
-    def normalize(self):
+    def normalize(self) -> None:
         """Calculate the normalization for the VDF."""
 
     @abstractmethod
-    def f_0(self):
-        """Return the values along the velocity axis of a VDF."""
+    def f_0(self) -> np.ndarray:
+        """Return the values along the velocity axis of a VDF.
+
+        Returns
+        -------
+        np.ndarray
+            1D array with the VDF values at the sampled points
+        """
 
 
 class VdfMaxwell(Vdf):
     """Create an object that make Maxwellian distribution functions.
 
-    Arguments:
-        VDF {ABC} -- abstract base class to make VDF objects
+    Parameters
+    ----------
+    VDF: ABC
+        Abstract base class to make VDF objects
     """
-
-    def __init__(self, params: config.Parameters, particle: config.Particle):
-        self.params = params
-        self.particle = particle
-        self.normalize()
-
-    def normalize(self):
-        self.A = (
-            2 * np.pi * self.particle.temperature * const.k / self.particle.mass
-        ) ** (-3 / 2)
-
-    def f_0(self):
-        return self.A * np.exp(
-            -self.particle.velocity_axis**2
-            / (2 * self.particle.temperature * const.k / self.particle.mass)
-        )
-
-
-class VdfKappa(Vdf):
-    """Create an object that make kappa distribution functions."""
 
     def __init__(self, params: config.Parameters, particle: config.Particle):
         """Initialize VDF parameters.
@@ -69,7 +59,40 @@ class VdfKappa(Vdf):
         self.particle = particle
         self.normalize()
 
-    def normalize(self):
+    def normalize(self) -> None:
+        self.A = (
+            2 * np.pi * self.particle.temperature * const.k / self.particle.mass
+        ) ** (-3 / 2)
+
+    def f_0(self) -> np.ndarray:
+        return self.A * np.exp(
+            -self.particle.velocity_axis**2
+            / (2 * self.particle.temperature * const.k / self.particle.mass)
+        )
+
+
+class VdfKappa(Vdf):
+    """Create an object that make kappa distribution functions.
+
+    Kappa VDF used in Gordeyev paper by Mace (2003).
+
+    """
+
+    def __init__(self, params: config.Parameters, particle: config.Particle):
+        """Initialize VDF parameters.
+
+        Parameters
+        ----------
+        params : Parameters
+            Parameters object with the parameters of the simulation.
+        particle : Particle
+            Particle object with the parameters of the particle.
+        """
+        self.params = params
+        self.particle = particle
+        self.normalize()
+
+    def normalize(self) -> None:
         self.theta_2 = (
             2
             * ((self.particle.kappa - 3 / 2) / self.particle.kappa)
@@ -84,15 +107,6 @@ class VdfKappa(Vdf):
         )
 
     def f_0(self) -> np.ndarray:
-        """Return the values along velocity `v` of a kappa VDF.
-
-        Kappa VDF used in Gordeyev paper by Mace (2003).
-
-        Returns
-        -------
-        np.ndarray
-            1D array with the VDF values at the sampled points
-        """
         return self.A * (
             1 + self.particle.velocity_axis**2 / (self.particle.kappa * self.theta_2)
         ) ** (-self.particle.kappa - 1)
