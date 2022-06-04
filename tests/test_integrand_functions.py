2e84ebe94114cf2a3725c467d4064272b2fe3926
engeir <eirroleng@gmail.com>
2022-06-04 22:21:26 +0200

    Re-write test: run methods instead of just checking they are defined

--- tests/test_integrand_functions.py
+++ tests/test_integrand_functions.py
@@ -2,16 +2,18 @@
 
 import inspect
 
+import inscar
 from inscar import integrand_functions as ins
 
 
 def test_contents():
     """Test that all classes contain needed methods and attributes."""
+    params = inscar.Parameters(frequency_size=1e3 + 1)
+    ion = inscar.Particle(gordeyev_size=1e3 + 1, velocity_size=1e3 + 1)
     for n, c in inspect.getmembers(ins, inspect.isclass):
         if c.__module__ == "inscar.integrand_functions" and n != "Integrand":
+            o = c()
             assert issubclass(c, ins.Integrand)
-            assert hasattr(c(), "the_type")
-            initialize = getattr(c(), "initialize", None)
-            assert initialize is not None
-            integrand = getattr(c(), "integrand", None)
-            assert integrand is not None
+            assert hasattr(o, "the_type")
+            o.initialize(params, ion)
+            o.integrand()
